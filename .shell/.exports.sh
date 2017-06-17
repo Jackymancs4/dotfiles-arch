6485c69cca38d2a434fe5257c30adad225d0fb3b
Giacomo Rossetto <jackyman_cs4@live.it>
2017-06-17 19:03:37 +0200

    Initial commit

---
+++ .shell/.exports.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Make vim the default editor.
+export EDITOR='vim';
+
+# Enable persistent REPL history for `node`.
+export NODE_REPL_HISTORY=~/.node_history;
+# Allow 32³ entries; the default is 1000.
+export NODE_REPL_HISTORY_SIZE='32768';
+# Make Python use UTF-8 encoding for output to stdin, stdout, and stderr.
+export PYTHONIOENCODING='UTF-8';
+
+# Increase Bash history size. Allow 32³ entries; the default is 500.
+export HISTSIZE='32768';
+export HISTFILESIZE="${HISTSIZE}";
+# Omit duplicates and commands that begin with a space from history.
+export HISTCONTROL='ignoreboth';
+
+# Entries beginning with space aren't added into history, and duplicate
+# entries will be erased (leaving the most recent entry).
+# export HISTCONTROL="ignorespace:erasedups"
+# Give history timestamps.
+export HISTTIMEFORMAT="[%F %T] "
+
+# Prefer US English and use UTF-8.
+export LANG='en_US.UTF-8';
+export LC_ALL='en_US.UTF-8';
+
+# Highlight section titles in manual pages.
+export LESS_TERMCAP_md="${yellow}";
+
+# Don’t clear the screen after quitting a manual page.
+export MANPAGER='less -X';
+
+# Always enable colored `grep` output.
+export GREP_OPTIONS='--color=auto';
---
+++ README.md
@@ -0,0 +1,29 @@
+# MAC
+
+https://github.com/nicksp
+
+## BREW
+
+the_silver_searcher
+terminal-notifier
+sl
+hub
+cmatrix
+git-extras
+
+## CASK
+
+suspicious-package
+quicklook-json
+quicklook-csv
+qlmarkdown
+
+
+
+---
+
+# UBUNTU
+
+## APT
+
+the_silver_searcher
